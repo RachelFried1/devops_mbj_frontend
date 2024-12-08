ac6fc600b2a5318bff7c9b86012190e1e6c17b57
RachelFried1 <rachelfried0828@gmail.com>
2024-12-08 23:11:44 +0200

    Change made

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>You are very welcome to Rachel-Fried React App!</h1>
+        <h1>Welcome to Rachel-Fried React App!</h1>
         <button /*onClick={fetchData}*/ className="fetch-button">Fetch backend</button>
         <p>{message}</p>
       </header>
